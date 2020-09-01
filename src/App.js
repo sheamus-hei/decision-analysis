e43b21c860f9d8779041d5e5d5aad8fa317c5355
deanjheikkila <deanjheikkila@gmail.com>
2020-08-31 17:01:43 -0700

    drew basic decision tree, next work on functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'react-tree-graph/dist/style.css'
 import './App.css';
 
+import Tree from 'react-tree-graph';
+
 function App() {
+  let data = {
+    name: 'Start',
+    children: [{
+        name: 'Do seismic survey -30',
+        children: [{
+          name: 'Unfavorable 70%',
+          children: [{
+            name: "Drill -100",
+            children: [{
+              name: "Oil 14.3% => Payoff 670"
+            }, {
+              name: "Dry 85.7% => Payoff -130"
+            }]
+          },{
+           name: "Sell +90 => Payoff 60" 
+          }]
+        },{
+          name: 'Favorable 30%',
+          children: [{
+            name: "Drill -100",
+            children: [{
+              name: "Oil 50% => Payoff 670"
+            }, {
+              name: "Dry 50% => Payoff -130"
+            }]
+          }, {
+            name: "Sell +90 => Payoff 60"
+          }]
+        }]
+    }, {
+        name: 'No Seismic survey +0',
+        children: [{
+          name: "Drill -100", 
+          children: [{
+            name: "Oil 25% => Payoff 700"
+          }, {
+            name: "Dry 75% => Payoff -100"
+          }]
+        }, {
+          name: "Sell +90 => Payoff 90"
+        }]
+    }]
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tree
+          data={data}
+          height={800}
+          width={900}/>
     </div>
   );
 }
